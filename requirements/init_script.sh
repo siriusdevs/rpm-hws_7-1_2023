f901c9a6811b1be683df639d697da382c9efe967
KatyaGotskina <katya.gotskina@gmail.com>
2023-03-22 14:41:54 +0300

    added script with docker conteiner

---
+++ requirements/init_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pip install -r requirements/requirements.txt
+
+# create docker container
+docker run -d \
+--name fate_server \
+-p 3850:5432 \
+-v $HOME/postgresql/fate_server:/var/lib.postgresql/fate_server \
+-e POSTGRES_PASSWORD=12345F \
+-e POSTGRES_USER=app \
+-e POSTGRES_DB=phrases_db \
+postgres
+sleep 2
+
+psql -h 127.0.0.1 -p 3850 -U app book_db -f requirements/db_creator.ddl
