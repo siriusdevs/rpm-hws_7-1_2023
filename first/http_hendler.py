87f9aa17deb65e192dddefca79826ae1b783fcde
kirillsev1 <kirillprinh47@gmail.com>
2023-03-23 09:58:51 +0300

    returning url with host and port

--- first/http_hendler.py
+++ first/http_hendler.py
@@ -142,7 +142,7 @@ class CustomHTTP(BaseHTTPRequestHandler):
             if not query:
                 return BAD_REQUEST, "DELETE FAILED"
             if DbHandler.delete(query):
-                return OK, f"{HOST}:{PORT}{self.path}: DELETE OK"
+                return OK, f"http://{HOST}:{PORT}{self.path}: DELETE OK"
         return NOT_FOUND, "Content not found"
 
     def put(self, record: dict = None) -> tuple:
@@ -163,7 +163,7 @@ class CustomHTTP(BaseHTTPRequestHandler):
                     return NOT_IMPLEMENTED, f"people do not have attribute: {attr}"
             if all([key in record for key in MAIN_REQUIRED_ATTRS]):
                 answer = "OK" if DbHandler.insert(record) else "FAIL"
-                return CREATED, f"{HOST}:{PORT}{self.path}: {self.command} {answer}"
+                return CREATED, f"http://{HOST}:{PORT}{self.path}: {self.command} {answer}"
             return BAD_REQUEST, f"Required keys to add: {MAIN_REQUIRED_ATTRS}"
         return NO_CONTENT, "Content not found"
 
@@ -185,7 +185,7 @@ class CustomHTTP(BaseHTTPRequestHandler):
             res = DbHandler.update(record=record, where=query)
             if not res:
                 return self.put(record)
-            return OK, f"{HOST}:{PORT}{self.path}: {self.command} OK"
+            return OK, f"http://{HOST}:{PORT}{self.path}: {self.command} OK"
 
     def get(self) -> None:
         """Runs get template method."""
