ab16e91125afb65b51e39e1aac7320027bb9c64a
annashutova <annashut@inbox.ru>
2023-03-24 01:25:37 +0300

    next time no more folders

--- lord_of_the_servers/tests/test.sh
+++ lord_of_the_servers/tests/test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # set env variables
-python3.10 lord_of_the_servers/tests/setup_env.py
+python3.10 tests/setup_env.py
 
 # set up database
-python3.10 lord_of_the_servers/tests/setup_db.py
+python3.10 tests/setup_db.py
 
 # server start
 echo "Starting the server"
-python3.10 lord_of_the_servers/main.py &
+python3.10 main.py &
 
 # real tests
 sleep 2
