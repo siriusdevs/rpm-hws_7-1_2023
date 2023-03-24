7e2121a4e1f78dcef8acdc4b26debee695cb5686
nazaroviktor2 <nazarov20012004@gmail.com>
2023-03-24 14:56:14 +0300

    try test #1

---
+++ image_api/tests/test_nasa_api.py
@@ -0,0 +1,52 @@
+"""Tests for nasa api."""
+from os import getenv
+
+
+from fastapi.testclient import TestClient
+
+from http_config import HTTP_UNPROCESS_ENTITY, HTTP_OK
+from src.main import app
+
+II = 2
+
+client = TestClient(app)
+TEST_USER = getenv("TEST_USER", default="test")
+TEST_TOKEN = getenv("TEST_TOKEN", default="aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee")
+USER_TOKEN = f"{TEST_USER}_{TEST_TOKEN}"
+IMAGE_ID = None
+
+
+def test_for_test():
+    assert II == 2
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == HTTP_OK
+
+
+def test_get_nasa_today_bad():
+    response = client.get('/image/nasa/')
+    assert response.status_code == HTTP_UNPROCESS_ENTITY
+
+
+def test_get_nasa_today():
+    response = client.get(f'/image/nasa/?token={USER_TOKEN}')
+    assert response.status_code == HTTP_OK
+
+
+def test_get_nasa_random():
+    response = client.get(f'/image/nasa/random/?token={USER_TOKEN}')
+    assert response.status_code == HTTP_OK
+
+
+def test_get_mars_today_bad():
+    response = client.get('/image/mars/')
+    assert response.status_code == HTTP_UNPROCESS_ENTITY
+
+
+def test_get_mars_today():
+    response = client.get(f'/image/mars/?token={USER_TOKEN}')
+    assert response.status_code == HTTP_OK
+
+
--- image_api/tests/test_user_image.py
+++ image_api/tests/test_user_image.py
@@ -1,5 +1,49 @@
-II = 1
+"""Tests for user image."""
+
+from os import getenv
+from pathlib import Path
+
+from fastapi.testclient import TestClient
+
+from http_config import  HTTP_OK, HTTP_CREATE, HTTP_FORBIDDEN
+from src.main import app
+
+II = 2
+
+client = TestClient(app)
+TEST_USER = getenv("TEST_USER", default="test")
+TEST_TOKEN = getenv("TEST_TOKEN", default="aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee")
+USER_TOKEN = f"{TEST_USER}_{TEST_TOKEN}"
+IMAGE_ID = None
 
 
-def test_for_test():
-    assert II == 1
+def test_user_post_bad():
+    with open(Path("tests/test_image.png").absolute(), 'rb') as test_file:
+        response = client.post(
+            f"/image/user/{TEST_USER}2/?token={USER_TOKEN}",  # wrong user
+            files={"upload_file": ("test.png", test_file, "image/png")},
+            params={"title": "test_image", "explanation": "image for testing"}
+        )
+    assert response.status_code == HTTP_FORBIDDEN
+
+
+def test_user_post():
+    global IMAGE_ID
+    with open(Path("tests/test_image.png").absolute(), 'rb') as test_file:
+        response = client.post(
+            f"/image/user/{TEST_USER}/?token={USER_TOKEN}",
+            files={"upload_file": ("test.png", test_file, "image/png")},
+            params={"title": "test_image", "explanation": "image for testing"}
+        )
+        IMAGE_ID = response.json().get("id")
+    assert response.status_code == HTTP_CREATE
+
+
+def test_user_get():
+    response = client.delete(f"/image/user/{TEST_USER}/{IMAGE_ID}/?token={USER_TOKEN}")
+    assert response.status_code == HTTP_OK
+
+
+def test_user_delete():
+    response = client.delete(f"/image/user/{TEST_USER}/{IMAGE_ID}/?token={USER_TOKEN}")
+    assert response.status_code == HTTP_OK
