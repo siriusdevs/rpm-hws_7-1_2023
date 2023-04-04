017dac6aa703cf6f18002a2c6f07444c4ed68e18
nester256 <nestvlad2004@gmail.com>
2023-04-04 13:26:41 +0300

    Kirill Prikhodko drinks coffee, which does not invigorate him, the question is why he drinks it

--- http_handler.py
+++ http_handler.py
@@ -86,6 +86,8 @@ class CustomHandler(BaseHTTPRequestHandler):
                 return BAD_REQUEST, 'DELETE FAILED'
             if DbHandler.delete(query):
                 return OK, f'Content has been deleted {self.path}'
+        else:
+            return BAD_REQUEST, "Invalid path"
         return NOT_FOUND, 'Content not found'
 
     # добавление
@@ -105,6 +107,8 @@ class CustomHandler(BaseHTTPRequestHandler):
                 answer = 'OK' if DbHandler.insert(content) else 'FAIL'
                 return CREATED, f'{self.command} {answer} {self.path}?name={content["name"]}'
             return BAD_REQUEST, f'Required keys to add: {IPS_REQUIRED_ATTRS}'
+        else:
+            return BAD_REQUEST, "Invalid path"
         return NO_CONTENT, 'Content not found'
 
     # Изменение
@@ -123,6 +127,8 @@ class CustomHandler(BaseHTTPRequestHandler):
             if not res:
                 return self.put()
             return OK, f'{self.command} OK {self.path}'
+        else:
+            return BAD_REQUEST, "Invalid path"
 
     def check_auth(self):
         auth = self.headers.get(AUTH, '').split()
