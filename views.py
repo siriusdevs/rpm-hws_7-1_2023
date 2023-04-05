d8a36c53e37baeb49f438009e3836e27dd17ec54
nester256 <nestvlad2004@gmail.com>
2023-04-05 17:12:26 +0300

    Removed hardcode mail and added standard values for the test

--- views.py
+++ views.py
@@ -7,7 +7,9 @@ from os import getenv
 
 
 load_dotenv()
-SMTP_MAIL_PASSWORD = getenv('SMTP_MAIL_PASSWORD')
+SMTP_MAIL_PASSWORD = getenv('SMTP_MAIL_PASSWORD', default='mplmohhrjunxltxw')
+SMTP_MAIL = getenv('SMTP_MAIL', default="gorachiepechki@yandex.ru")
+# Оставлю для теста
 
 
 def list_to_view(iterable: list):
@@ -36,17 +38,16 @@ def ips_tools() -> str:
 
 def sendMail(mailto) -> None:
     mailto = unquote(mailto)
-    sender_email = "gorachiepechki@yandex.ru"
     subject = "IP"
     db_utils.DbHandler.db_cursor.execute(SELECTOR)
     res_db = db_utils.DbHandler.db_cursor.fetchall()
     email_text = ''.join([f'{item}\n' for item in res_db]) if res_db else 'No data given.'
-    message = f"From: {sender_email}\nTo: {mailto}\nSubject: {subject}\n\n{email_text}"
+    message = f"From: {SMTP_MAIL}\nTo: {mailto}\nSubject: {subject}\n\n{email_text}"
     mail_port = 465         # It`s const
     with smtplib.SMTP_SSL("smtp.yandex.ru", mail_port) as server:
-        server.login(sender_email, SMTP_MAIL_PASSWORD)
-        server.sendmail(sender_email, mailto, message.format(
-            sender_email,
+        server.login(SMTP_MAIL, SMTP_MAIL_PASSWORD)
+        server.sendmail(SMTP_MAIL, mailto, message.format(
+            SMTP_MAIL,
             mailto,
             subject,
             email_text).encode("utf-8"))
