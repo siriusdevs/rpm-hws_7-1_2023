ebd601c5b16471dc900a2313891de3b3a4fb3828
rtrwslv <83144402+rtrwslv@users.noreply.github.com>
2023-04-06 13:12:29 +0300

    Deleted some useless files

--- auth.py
+++
@@ -1,8 +0,0 @@
-from flask import request
-
-if request.method == "POST":
-       # getting input with name = fname in HTML form
-       first_name = request.form.get("fname")
-       # getting input with name = lname in HTML form
-       last_name = request.form.get("lname")
-       return "Your name is "+first_name + last_name
