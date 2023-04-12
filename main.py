425dda7cf88f8da4d96dbc450e555e81bf614e29
rtrwslv <83144402+rtrwslv@users.noreply.github.com>
2023-04-12 13:12:27 +0300

    Added notification if db is not connected

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from http.server import HTTPServer
-from http_handler import CustomHandler
+from os import getenv
+import psycopg2
 from config import HOST, PORT
-
+from http_handler import CustomHandler
 
 if __name__ == '__main__':
-    with HTTPServer((HOST, PORT), CustomHandler) as server:
-        server.serve_forever()
+    try:
+        db_connection = psycopg2.connect(
+        dbname=getenv("PG_DBNAME"),
+        host=getenv("PG_HOST"),
+        port=getenv("PG_PORT"), user=getenv("PG_USER"),
+        password=getenv("PG_PASSWORD"),
+        )
+        cursor = db_connection.cursor()
+    except psycopg2.OperationalError:
+        print("Не подключена база данных")
+    else:
+        with HTTPServer((HOST, PORT), CustomHandler) as server:
+            server.serve_forever()
