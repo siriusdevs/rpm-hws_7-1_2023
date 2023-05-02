63c43562221237eb101e85d025400faec26a7e71
rtrwslv <83144402+rtrwslv@users.noreply.github.com>
2023-05-02 15:24:48 +0300

    linter changes

--- config.py
+++ config.py
@@ -18,15 +18,5 @@ NOT_FOUND = 404
 FORBIDDEN = 403
 BAD_REQUEST = 400
 
-# db requests
-SELECTOR = 'SELECT * FROM students'
-GET_TOKEN = 'SELECT token FROM token WHERE username=\'{username}\''
-INSERT = 'INSERT INTO group_{group_num} VALUES (\'{name}\')'
-DELETE = 'DELETE FROM group_{group_num} WHERE name=\'{name}\''
-
 # page str to byte coding
 CODING = 'KOI8-R'
-
-# weather consts
-COLLEGE_LOCATION = {'lat': 43.403438, 'lon': 39.981544}
-API_URL = 'https://geek-jokes.sameerkumar.website/api?format=json'
