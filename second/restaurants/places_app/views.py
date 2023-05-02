34b7dbc9b4440d3e9358fba7a3bff0d30b35da9e
nester256 <nestvlad2004@gmail.com>
2023-05-02 17:42:51 +0300

    Change flake8 errors

--- second/restaurants/places_app/views.py
+++ second/restaurants/places_app/views.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
