25e834c3dcd8087b23fbd43490ccc48cce771228
Nirtkor <itnirtkor@gmail.com>
2023-06-08 18:30:29 +0300

    fix api.py

--- api.py
+++ api.py
@@ -1,21 +1,27 @@
+"""Файл для работы с API, получает sol_date, выдает рендер для шаблона"""
 import requests
+
 from jinja2 import Environment, FileSystemLoader
 
+
 def api_module(sol_date):
+    """Метод делает запрос к API, получая дату для обращения
+    возвращает рендер изображения для шаблона
+
+    Args:
+        sol_date (int): дата для вывода рендера
+
+    Returns:
+        photo: рендер для шаблона
     """
-    Получает фотографию с API NASA для указанной даты sol_date.
-    :param sol_date: Дата для получения фотографии (sol).
-    :return: Рендеринг шаблона с фотографией.
-    """
-    env = Environment(loader=FileSystemLoader('.'))
+    env = Environment(loader=FileSystemLoader('.'), autoescape=True)
     template = env.get_template('template/info.html')
-    endpoint = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos'
+    endpoint='https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos'
     api_key = 'YGjdEiolRdZV5T4dDZuq2ifq1ECuPk8PRmKYSueJ'
     query_params = {'api_key': api_key, 'sol': sol_date}
     response = requests.get(endpoint, params=query_params)
-    data = response.json()
+    photo_data = response.json()
     try:
-        photo = data['photos'][0]['img_src']
-        return template.render(photo=photo)
+        return template.render(photo=photo_data['photos'][0]['img_src'])
     except IndexError:
         return None
