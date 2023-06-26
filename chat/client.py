3857a8c807519898307358868bc15d7506c03bef
rtrwslv <83144402+rtrwslv@users.noreply.github.com>
2023-06-27 00:29:16 +0300

    linter update

--- chat/client.py
+++ chat/client.py
@@ -9,9 +9,7 @@ tcpclient_a = None
 
 class AuthorizationPage(QWidget):
     def __init__(self):
-        """
-        Creates window for authorization
-        """
+        """Creates window for authorization"""
         super().__init__()
         self.setWindowTitle("Authorization Page")
         self.setFixedSize(300, 150)
@@ -60,9 +58,9 @@ class MainWindow(QWidget):
         self.chatTextField = QLineEdit(self)
         self.chatTextField.resize(480, 100)
         self.chatTextField.move(10, 350)
-        self.btnSend=QPushButton("Send",self)
+        self.btnSend = QPushButton("Send", self)
         self.btnSend.resize(480, 30)
-        self.btnSendFont=self.btnSend.font()
+        self.btnSendFont = self.btnSend.font()
         self.btnSendFont.setPointSize(15)
         self.btnSend.setFont(self.btnSendFont)
         self.btnSend.move(10, 460)
@@ -75,7 +73,7 @@ class MainWindow(QWidget):
         splitter.addWidget(self.chat)
         splitter.addWidget(self.chatTextField)
         splitter.setSizes([400, 100])
-        splitter2=QSplitter(QtCore.Qt.Vertical)
+        splitter2 = QSplitter(QtCore.Qt.Vertical)
         splitter2.addWidget(splitter)
         splitter2.addWidget(self.btnSend)
         splitter2.setSizes([200, 10])
@@ -88,31 +86,31 @@ class MainWindow(QWidget):
         font = self.chat.font()
         font.setPointSize(13)
         self.chat.setFont(font)
-        text_formatted = 'U: ' + text
+        text_formatted = f'U: {text}'
         print(text_formatted)
         self.chat.append(text_formatted)
         tcpclient_a.send(text.encode())
         self.chatTextField.setText("")
 
 
-class client_thread(Thread):
+class ClientThread(Thread):
     def __init__(self, window):
         """
         Creates thread for client
         """
-        Thread.__init__(self)
+        super().__init__(self)
         self.window = window
 
     def run(self):
-        global tcpclient_a
+        #global tcpclient_a
         host = socket.gethostname()
         port = 80
-        BUFFER_SIZE = 2000
-        tcpclient_a = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-        tcpclient_a.connect((host, port)) 
+        buffer_size = 2000
+        tcpclient_a = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcpclient_a.connect((host, port))
         while True:
-            data = tcpclient_a.recv(BUFFER_SIZE)
-            self.window.chat.append("server: " + data.decode("utf-8"))
+            data = tcpclient_a.recv(buffer_size)
+            self.window.chat.append(f"server: {data.decode('utf-8')}")
 
 
 if __name__ == '__main__':
--- chat/server.py
+++ chat/server.py
@@ -1,29 +1,28 @@
-import sys,time
-from PyQt5 import QtGui
+import sys
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QScrollBar,QSplitter,QTableWidgetItem,QTableWidget,QComboBox,QVBoxLayout,QGridLayout,QDialog,QWidget, QPushButton, QApplication, QMainWindow,QAction,QMessageBox,QLabel,QTextEdit,QProgressBar,QLineEdit
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtWidgets import QSplitter, QVBoxLayout, QDialog, QPushButton, QApplication, QTextEdit, QLineEdit
 import socket
-from threading import Thread 
-from socketserver import ThreadingMixIn 
+from threading import Thread
 
-conn=None
+conn = None
+
+
 class Window(QDialog):
     def __init__(self):
         """
         Creates window for chat
         """
         super().__init__()
-        self.flag=0
+        self.flag = 0
         self.chatTextField=QLineEdit(self)
-        self.chatTextField.resize(480,100)
-        self.chatTextField.move(10,350)
+        self.chatTextField.resize(480, 100)
+        self.chatTextField.move(10, 350)
         self.btnSend=QPushButton("Send",self)
-        self.btnSend.resize(480,30)
+        self.btnSend.resize(480, 30)
         self.btnSendFont=self.btnSend.font()
         self.btnSendFont.setPointSize(15)
         self.btnSend.setFont(self.btnSendFont)
-        self.btnSend.move(10,460)
+        self.btnSend.move(10, 460)
         self.btnSend.setStyleSheet("background-color: #8e7cc3")
         self.btnSend.clicked.connect(self.send)
         self.chatBody=QVBoxLayout(self)
@@ -32,11 +31,11 @@ class Window(QDialog):
         self.chat.setReadOnly(True)
         splitter.addWidget(self.chat)
         splitter.addWidget(self.chatTextField)
-        splitter.setSizes([400,100])
+        splitter.setSizes([400, 100])
         splitter2=QSplitter(QtCore.Qt.Vertical)
         splitter2.addWidget(splitter)
         splitter2.addWidget(self.btnSend)
-        splitter2.setSizes([200,10])
+        splitter2.setSizes([200, 10])
         self.chatBody.addWidget(splitter2)
         self.setWindowTitle("Chat Application: Server")
         self.resize(500, 500)
@@ -46,7 +45,7 @@ class Window(QDialog):
         font=self.chat.font()
         font.setPointSize(13)
         self.chat.setFont(font)
-        textFormatted='U:' + text
+        textFormatted = f'U:{text}'
         self.chat.append(textFormatted)
         global conn
         conn.send(text.encode("utf-8"))
@@ -61,7 +60,6 @@ class ServerThread(Thread):
     def run(self): 
         TCP_IP = '0.0.0.0' 
         TCP_PORT = 80 
-        BUFFER_SIZE = 20  
         tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
         tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
         tcpServer.bind((TCP_IP, TCP_PORT)) 
